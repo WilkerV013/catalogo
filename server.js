3cf3e1f1eb8397520197e073d16a79b8838b52cb
WilkerV013 <wilkerdevasconcelos@gmail.com>
2021-12-28 22:43:25 -0300

    make it better

---
+++ server.js
@@ -0,0 +1,16 @@
+const express =require ('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/dist/angular-heroes'));
+
+app.get('/*',(req,res)=>{
+    res.sendFile(__dirname + 'dist/angular-heroes/index.html');
+});
+
+app.listen(PORT, () => {
+    console.log('servidor iniciado na porta '+ PORT);
+});
