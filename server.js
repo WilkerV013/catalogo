544dcdef7d1c562f88eefac52d3e6c7c414c3f8f
WilkerV013 <wilkerdevasconcelos@gmail.com>
2021-12-29 21:00:00 -0300

    atualizando server

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-const express = require ('express');
+const express = require('express');
 const path = require('path');
-
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+app.use(express.static(`${__dirname}/dist/${angular-heroes}`));
 
-app.use(express.static(__dirname + '/dist/angular-heroes'));
-
-app.get('/*',(req,res)=>{
-    res.sendFile(__dirname + 'dist/angular-heroes/index.html');
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${angular-heroes}/index.html`));
 });
 
-app.listen(PORT, () => {
-    console.log('servidor iniciado na porta'+ PORT);
-});
+app.listen(process.env.PORT || 8080);
